819521d6fc560d6354a427969834663d1d463ba4
Ishaan1304 <109105499+Ishaan1304@users.noreply.github.com>
2024-06-04 00:46:34 +0530

    Add files via upload

---
+++ student-management-system/src/main/java/com/manager/student/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.manager.student.service;
+
+import java.util.List;
+
+import com.manager.student.entity.Student;
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+	
+	Student updateStudent(Student student);
+	
+	void deleteStudentById(Long id);
+}
